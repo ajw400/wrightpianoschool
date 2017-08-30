53e89c54371dce1ed1a1e42d469dda518b45293b
Andrew <prestoassai@hotmail.com>
2017-08-30 13:23:48 +0200

    initial commit

---
+++ db/migrate/20170830111625_create_teachers.rb
@@ -0,0 +1,15 @@
+class CreateTeachers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :teachers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.string :email
+      t.text :bio
+      t.integer :hour_fee
+      t.integer :in_home_fee
+
+      t.timestamps
+    end
+  end
+end
