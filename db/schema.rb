2f9a42c0515ec3a8fa7bd38b3ee127f3bef23ff3
Andrew <prestoassai@hotmail.com>
2018-04-23 12:34:57 +0200

    add name to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930120533) do
+ActiveRecord::Schema.define(version: 20180423103343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170930120533) do
     t.datetime "updated_at", null: false
     t.string   "email"
     t.string   "phone"
+    t.string   "name"
     t.index ["teacher_id"], name: "index_messages_on_teacher_id", using: :btree
   end
 
