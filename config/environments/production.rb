fa710d67c27a3f316e1cf46fc411d291f62281ab
Andrew <prestoassai@hotmail.com>
2018-04-23 12:10:47 +0200

    settings'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "wrightpianoschool_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "www.wrightpianoschool.com" }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: "www.wrightpianoschool.com" }
   # or your custom domain name eg. "www.yourdomain.com"
 
 
