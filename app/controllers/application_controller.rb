dae5199cf4828d43cecb4affb11a934a3470737c
Andrew <prestoassai@hotmail.com>
2019-01-21 20:01:09 +0100

    add localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :set_signup_from_params
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   def set_signup_from_params
     @signup = true if params[:signup] == "true"
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,26 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'pages#home'
   mount Attachinary::Engine => "/attachinary"
-  resources :teachers
-  resources :messages, only: [:create]
-  resource :pages, only: [] do
-    member do
-      get :about
-      get :contact
-      get :policy
-      get :calendar
-      get :download_policy
-      get :download_calendar
-      get :videos
+
+  scope '(:locale)', locale: /en|nl/ do
+    root to: 'pages#home'
+    resources :teachers
+    resources :messages, only: [:create]
+    resource :pages, only: [] do
+      member do
+        get :about
+        get :contact
+        get :policy
+        get :calendar
+        get :download_policy
+        get :download_calendar
+        get :videos
+      end
     end
+    resources :paragraphs, only: [:create, :update, :destroy]
+    resources :degrees, only: [:create, :update, :destroy]
+    resources :students, only: [:new, :create, :update, :destroy]
   end
-  resources :paragraphs, only: [:create, :update, :destroy]
-  resources :degrees, only: [:create, :update, :destroy]
-  resources :students, only: [:new, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
